7425f05eeb02773161abf701624636868a6201d0
queso1307 <queso1307@googlemail.com>
2013-10-09 13:38:01 +0200

    Added rudimentary payload analyse

--- mqtt_client.py
+++ mqtt_client.py
@@ -3,22 +3,37 @@
 
 import json
 import mosquitto
+# function for setting output values
+def set_value(pin, value):
+	print(pin, value)
+	
+
 
 def on_connect(mosq, obj, rc):
-    mosq.subscribe(topic, 0)
-    print("rc: "+str(rc))
+	mosq.subscribe(topic, 0)
+	print("rc: "+str(rc))
 
 def on_message(mosq, obj, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+#	print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+#saving payload and searching for keywords
+	payload = str(msg.payload)
+	print payload
+	if payload.startswith("MS_CONV_1"):
+		payload_list = payload.split()	
+		pin = str(payload_list[0])
+		value = int(payload_list[1])
+		set_value(pin, value)
+
+
 
 def on_publish(mosq, obj, mid):
-    print("mid: "+str(mid))
+	print("mid: "+str(mid))
 
 def on_subscribe(mosq, obj, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+	print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
 def on_log(mosq, obj, level, string):
-    print(string)
+	print(string)
 
 
 
